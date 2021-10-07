7d7c90d1e33c62468fcbb4ea57eacdcba91e9731
AkhilMylaram <akhilmylaram123@gmail.com>
2021-10-07 23:41:18 +0530

    TwoDimensionalArray

---
+++ src/assignments/array2d.java
@@ -0,0 +1,26 @@
+package assignments;
+
+import java.util.Scanner;
+
+public class array2d {
+    public static void main(String[] args) {
+        int index=1;
+        int[][] a =new int[2][2];
+        Scanner sc=new Scanner(System.in);
+       for(int i=0;i<index;i++)
+       {
+           for(int j=0;j<index;j++)
+           {
+               System.out.println("enter the number:");
+               a[i][j]=sc.nextInt();
+           }
+       }
+        for (int[] ints : a) {
+            for (int j = 0; j < a.length; j++) {
+                System.out.print(ints[j]);
+
+            }
+            System.out.println();
+        }
+    }
+}
